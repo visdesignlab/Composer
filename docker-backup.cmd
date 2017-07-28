5a6e2b997548bf228fb6da3cab3d626b01fb32dd
saharmehrpour <mehrpour@cs.utah.edu>
2017-07-27 21:54:20 -0400

    merge with the master

--- docker-backup.cmd
+++ docker-backup.cmd
@@ -12,14 +12,14 @@ if "%1"=="backup" (
   if "%2"=="" (
     call :backup
   ) else (
-    call :backup_one %2%
+    call :backup_one %PREFIX%_%1%
   )
 )
 if "%1"=="restore" (
   if "%2"=="" (
     call :restore
   ) else (
-    call :restore_one %2%
+    call :restore_one %PREFIX%_%1%
   )
 )
 
