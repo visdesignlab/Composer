6c52b464c3f1d6bbd7af17551ac8b64e77011617
Jen Rogers <jennifer.rogers1207@gmail.com>
2017-10-27 18:17:51 -0600

    added proposal

--- client_app/src/patOrderBreakdown.ts
+++ client_app/src/patOrderBreakdown.ts
@@ -125,16 +125,17 @@ export class patOrderBreakdown {
       this.similarPatientsProInfo = [];
 
       this.setOrderScale();
-
+     this.loadDataFromServer();
+     this.basicTableUsage();
      // this.drawPatOrderRects(this.targetPatientOrders);
-  
+     const allDatasets = listData();
+     //console.log('All loaded datasets:');
+     console.log(allDatasets);
     });
 
 
   }
 
- 
- 
   private assignCurrentName (d) {
           
          
@@ -276,38 +277,91 @@ export class patOrderBreakdown {
     // notice the await keyword - you'll see an explanation below
     const allDatasets = await listData();
     //console.log('All loaded datasets:');
-    console.log(allDatasets);
+    console.log("datasets "+allDatasets);
      // we could use those dataset to filter them based on their description and pick the one(s) we're interested in
     // here we pick the first dataset and cast it to ITable - by default the datasets are returned as IDataType
     let tempTable: ITable;
     let orderArray = [];
     // retrieving a dataset by name. Note that only the first dataset will be returned.
-    tempTable = <ITable> await getById('Orders');
-   
-    let tempVector = await tempTable.cols()[5].data();
-    console.log(tempVector);
+    //tempTable = <ITable> await getById('Orders');
+    //tempTable = <ITable> await getById('ICD_codes');
+  //  let tempVector = await tempTable.cols()[5].data();
+    //console.log("tempvector?? "+tempVector);
+    //console.log(tempTable.dim);
     //console.log(this.currentlySelectedName);
 
    }
 
-/*
- const url = `/data_api/getSimilarRows/${value}/${n}/${this.dataset}`;
-            this.setBusy(true);
-            this.getData(url).then((args) => {
-                this.setBusy(false);
-                this.similarArgs = args;*/
+   public async basicTableUsage() {
+    let table : ITable;
 
-  /**
-   * Show or hide the application loading indicator
-   * @param isBusy
-   */
-  setBusy(isBusy: boolean) {
-    let status = select('.busy').classed('hidden');
-    if (status == isBusy)
-      select('.busy').classed('hidden', !isBusy);
+    //this.table = <ITable> await getById('CPT_codes');
+    this.table = <ITable> await getById('Demo_selected');
+  
+
+    // Accessing the description of the dataset:
+    console.log('Table description:');
+    console.log(this.table.desc);
+
+    console.log('=============================');
+    console.log('FILTERING BASED ON DATA');
+    console.log('=============================');
+    console.log(this.table.col(3).valuetype.type);
+
+    console.log('=============================');
+    console.log('FILTERING BASED ON DATA');
+    console.log('=============================');
+    console.log('Extracting the Number of Studio Albums column:');
+    let patID: INumericalVector;
+    this.table.cols().forEach((vector: IAnyVector) => {
+      if (vector.desc.name === 'PAT_ID') {
+        patID = <INumericalVector>vector;
+      }
+    });
+    console.log(await patID.data());
+
+    if (patID == null) {
+      return;
+    }
+
+    const filteredID: INumericalVector = await patID.filter((d: number, i: number) => {
+      if (d > 4000) {
+        return true;
+      }
+      return false;
+    });
+    console.log('The data of the vector filtered by number of albums > 8');
+    console.log(await filteredID.data());
+
+    const ids = await filteredID.ids();
+
+    const tableView: ITable = await this.table.idView(ids);
+    console.log('The data of the table filtered by number of albums > 8');
+    console.log(await tableView.data());
+
+    console.log('-----------');
+    console.log('Using argFilter to get the indices of a filter operation:');
+    console.log('-----------');
+    const albumData = await patID.data();
+    const indices = argFilter(albumData, (d) => d > 8);
+    console.log('The data of the table filterd by number of albums > 8');
+    console.log(await this.table.data(indices));
+
+    console.log('-----------');
+    console.log('Using the filter operation of the range');
+    console.log('-----------');
+    // list creates an index range, and we apply this list to the array of data passed
+    console.log(list(indices).filter(await this.table.data()));
   }
-
+  }
 }
+
+
+   
+
+
+
+   
 
 export function create(parent:Element) {
   return new patOrderBreakdown(parent);
