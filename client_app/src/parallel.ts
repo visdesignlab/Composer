7b2998392e2d5c6e950e3c65c4633d946290d8d6
Jen Rogers <jennifer.rogers1207@gmail.com>
2017-11-07 13:36:27 -0700

    pushing changes befor I destroy everything

--- client_app/src/parallel.ts
+++ client_app/src/parallel.ts
@@ -14,7 +14,7 @@ import {drag} from 'd3-drag';
 import {Constants} from './constants';
 import * as dataCalc from './dataCalculations';
 import {transition} from 'd3-transition';
-import {brush, brushY, brushX} from 'd3-brush';
+import {brush, brushY, brushX, brushSelection} from 'd3-brush';
 import * as similarityScore  from './similarityScoreDiagram';
 import * as d3 from 'd3';
 import {ITable, asTable} from 'phovea_core/src/table';
@@ -31,35 +31,29 @@ import {argFilter} from 'phovea_core/src/';
 export class parallel {
 
   private $node;
-  private timeScale;
-  private scoreScale;
-  private timeScaleMini;
-  private patientInfo;
   private svg;
- // private targetPatientProInfo;
   private brush;
-  private targetPatientOrders;
-  private currentlySelectedName;
-  private similarData;
   private allData;
- // private selectedOrder;
-  private plotLines;
-
   private yscale;
   private xscale;
-  private similarPatientsProInfo;
   private plotDimension = {width: 1100, height: 300 };
   private margin = {top: 20, right: 10, bottom: 10, left: 10};
+  private SelectedCounter;
+  private plotLines;
+  private backgroundLines;
+  private dimension;
+  private path;
 
-  private filter1;
-
+  selectedData;
+  contextData;
+  
 
   table: ITable;
 
   constructor(parent: Element) {
 
     this.$node = select(parent)
-        .append('div')//.attr('height', 600)
+        .append('div')
         .classed('parallelDiv', true);
     
     this.svg = this.$node.append('svg')
@@ -67,16 +61,26 @@ export class parallel {
         .attr('width', this.plotDimension.width )
         .attr('height', this.plotDimension.height + this.margin.top + this.margin.bottom);
     
-    this.timeScale = scaleLinear()
-        .range([0, this.plotDimension.width])
-        .clamp(true);
-
     this.yscale = scaleLinear().range([0, this.plotDimension.height]);
     this.xscale = scaleLinear().range([0, this.plotDimension.width]);
 
-    this.plotPatients();
+    this.mapPatientData();
+    this.attachListener();
+    
     
   }
+
+  private attachListener() {
+    
+            // item: pat_id, number of similar patients, DATA
+            events.on('filter_data', (evt, item) => { // called in queryBox
+              console.log(event);
+              console.log(item);
+              this.updatePlot(this.plotLines, item);
+
+            });
+    
+        }
   
 
     /**
@@ -88,13 +92,10 @@ export class parallel {
         return await ajax.getAPIJSON(URL);
       }
 
-  public async plotPatients() {
-
-   // const allDatasets = await listData();
-   // console.log('All loaded datasets:');
-   // console.log(allDatasets);
+  public async mapPatientData() {
 
     let table : ITable;
+    let that = this;
 
     //this.table = <ITable> await getById('CPT_codes');
   this.table = <ITable> await getById('Demo_Info');
@@ -106,6 +107,7 @@ export class parallel {
      let TOBACCO = (await this.table.colData('TOBACCO_USER')).map(d => d);
      let ALCOHOL = (await this.table.colData('ALCOHOL_USER')).map(d => d);
      let DRUG_USER = (await this.table.colData('ILLICIT_DRUG_USER')).map(d => d);
+     let RACE = (await this.table.colData('PAT_RACE')).map(d => d);
      let BMI = (await this.table.colData('BMI')).map(d => +d);
      let patDOB = (await this.table.colData('PAT_BIRTHDATE')).map(d => new Date(String(d)));
      let CCI = (await this.table.colData('CCI')).map(d => +d);
@@ -118,7 +120,7 @@ export class parallel {
        
       });
   
-      let rows = patID.map((id, i) => {
+      this.selectedData = patID.map((id, i) => {
         return {
           ID: id,
           GENDER: GENDER[i],
@@ -128,101 +130,183 @@ export class parallel {
           TOBACCO: TOBACCO[i],
           ALCOHOL: ALCOHOL[i],
           DRUG_USER: DRUG_USER[i],
+          RACE : RACE[i],
           CCI: CCI[i]
         };
       });
 
-      console.log(rows);
+  this.dimension = [
 
-  let dimension = [
-
-    //  {'value': 'ID', 'data': patID, 'scale': this.yscale.domain(d3.extent(patID))},
-     // {'value' : 'AGE', 'data':  patAge, 'scale': this.yscale.domain(d3.extent(patAge)) },
-     {'value' : 'AGE', 'data':  patAge, 'scale': scaleLinear().range(this.yscale.range()).domain([d3.max(patAge),d3.min(patAge)]) },
-      {'value': 'BMI', 'data': BMI, 'scale': scaleLinear().range(this.yscale.range()).domain([d3.max(BMI),d3.min(BMI)]) },
-      {'value': 'CCI', 'data': CCI, 'scale': scaleLinear().range(this.yscale.range()).domain([d3.max(CCI),d3.min(CCI)]) }
+     {'value' : 'AGE', 'data':  patAge, 'scale': scaleLinear().range(this.yscale.range()).domain([d3.max(patAge),d3.min(patAge)]).clamp(true) },
+      {'value': 'BMI', 'data': BMI, 'scale': scaleLinear().range(this.yscale.range()).domain([d3.max(BMI),d3.min(BMI)]).clamp(true) },
+      {'value': 'CCI', 'data': CCI, 'scale': scaleLinear().range(this.yscale.range()).domain([d3.max(CCI),d3.min(CCI)]).clamp(true) }
 
     ];
  
   let lineGen = d3.svg.line();
-  
+  this.xscale.domain([0, this.dimension.length]);
+  let axis = d3.svg.axis().orient("left");
 
-  var xtest = scaleLinear().range([0, this.plotDimension.width]).domain([0, dimension.length]);
+  this.path = (d) => {
+    
+      let coordinateList = this.dimension.map((p, i) => {
 
-     let axis = d3.svg.axis().orient("left");
-
-  let path = (d) => {
-
-      let coordinateList = dimension.map((p, i) => {
-
-        if(p.value !== d.ID && 
-           p.value !== d.TOBACCO && 
-           p.value !== d.ALCOHOL && 
-           p.value !== d.GENDER && 
-           p.value !== d.PAT_MARITAL_STAT &&
-           p.value !== d.DRUG_USER
+        if(p.value !== d.ID //&& 
+          // p.value !== d.TOBACCO && 
+          // p.value !== d.ALCOHOL && 
+          // p.value !== d.GENDER && 
+          // p.value !== d.PAT_MARITAL_STAT &&
+          // p.value !== d.DRUG_USER &&
+          // p.value !== d.RACE
           ){
       
-             let x = scaleLinear().domain([0, dimension.length]).range([0, this.plotDimension.width]);
-          
-              let axisXCoord = x(i);
+              let axisXCoord = this.xscale(i);
               let yScale = p.scale;
               let lineYCoord = yScale(d[p.value]);
-             // console.log(d[p.value]);
         
               return [axisXCoord, lineYCoord];
         }
             });
           
             // coordinateList is a list of lists
-            //console.log(coordinateList);
             return lineGen(coordinateList);
-
   }
 
-  this.plotLines = this.svg.append('g').attr('height', this.plotDimension.height).attr('transform', 'translate(25, '+this.margin.top+')')
-  .attr('id', 'plotGroup').selectAll('path').data(rows);
-
-  let linesEnter = this.plotLines.enter().append('path');
-  this.plotLines = this.plotLines.merge(linesEnter);
-  this.plotLines.attr('d', d => path(d));
-
-  this.plotLines.attr('fill', 'none').attr('stroke', 'black').attr('stroke-width', .3).attr("stroke-opacity", 0.2);
-  this.plotLines.on('mouseover', (d)=> console.log(d));
      // Add a group element for each dimension.
      
-  let g = this.svg.selectAll(".dimension")
-  .data(dimension)
+  let dimensionGroup = this.svg.selectAll(".dimension")
+  .data(this.dimension)
   .enter().append("g")
   .attr("class", "dimension")
-  .attr("transform", function(d, i) { return "translate("+ xtest(i) +")"; });
+  .attr("transform", function(d, i) { return "translate("+ that.xscale(i) +")"; });
 
    // Add an axis and title.
-   g.append("g").attr('transform', 'translate(25, '+this.margin.top+')')
+   dimensionGroup.append("g").attr('transform', 'translate(25, '+this.margin.top+')')
    .attr("class", "axis")
    .each(function(d, i) { d3.select(this).call(axis.scale(d.scale)); })
- .append("text")
+   .append("text")
    .style("text-anchor", "middle")
    .attr("y", -9)
    .text(function(d) { return d.value; });
-  
+
+   this.brush = brushY()
+   .extent([[0, 0], [25, this.plotDimension.height + this.margin.top]])
+
+   dimensionGroup.append("g")
+   .attr("class", "brush")
+   .call(this.brush);
+
+   // Handles a brush event, toggling the display of foreground lines.
+   let brushed = function() {
+    console.log('brushed');
+    }
+
+    let brush = function() {
+      
+           // render.invalidate();
+          var actives = [];
+          dimensionGroup.selectAll(".brush")
+            .filter(function(d) {
+              return brushSelection(this);
+            })
+            .each(function(d) {
+              actives.push({
+                dimension: d,
+                extent: brushSelection(this)
+              });
+            });
+
+      
+          var selected = this.plotLines.filter(function(d) {
+            if (actives.every(function(active) {
+
+               var dim = active.dimension;
+               console.log(d);
+                // test if point is within extents for each active brush
+               return dim.type.within(d[dim.key], active.extent, dim);
+
+              })) 
+              {
+              return true;
+            }
+          });
+
+      
+        }
+
+this.brush.on('end', brush);
+
+this.plotPatients(this.selectedData);
+
+this.SelectedCounter = this.svg.append('g')
+.attr('transform', 'translate(30,' + (this.plotDimension.height + 30) + ')')
+.classed('counter', true).append('text');
+
+
 }
+
+private plotPatients(data){
+
+  //console.log(this.selectedData);
+  this.plotLines = this.svg.append('g')
+  .attr('height', this.plotDimension.height).attr('transform', 'translate(25, '+this.margin.top+')')
+  .attr('id', 'plotGroup').selectAll('path').data(data);
+
+   // Add grey background lines for context.
+   /*
+   this.backgroundLines = this.svg.append('g')
+   .attr('class', "background")
+    .selectAll('path')
+   .data(this.contextData)
+   .enter().append("path")
+   .attr("d", path);*/
+
+  let linesEnter = this.plotLines.enter().append('path');
+  this.plotLines = this.plotLines.merge(linesEnter);
+
+  this.plotLines.exit().remove();
+
+  this.plotLines.attr('d', d => this.path(d));
+
+  this.plotLines.attr('fill', 'none').attr('stroke', 'black').attr('stroke-width', .3).attr("stroke-opacity", 0.1);
+  this.plotLines.on('mouseover', (d)=> console.log(d));
+
+
+}
+
+private updatePlot(selected, d) {
+
+let choice = d[0];
+let parent = d[1];
+
+    let lines = select('#plotGroup').selectAll('path');
+
+    // let filterGroup = selected.filter(d => d[parent] == choice);
+
+    // let reject = lines.filter(d => d[parent] == choice);
+
+     let filteredData = this.selectedData.filter(d => d[parent] == choice);
+
+     let rejectData = this.selectedData.filter(d => d[parent] !== choice);//.classed('hidden', true);
+     console.log(rejectData);
+     console.log(filteredData);
+
+   // filterGroup.classed(parent, true);
+
+  //  this.plotLines = filterGroup;
+
+  //  console.log(filterGroup);
+   // console.log(reject);
+
+    this.SelectedCounter.text('Selected Patients:   ' + this.plotLines.size());
+
+    this.plotPatients(filteredData);
+
+}
+
+
   }
 
-  export function filterPlot(d, p) {
-
-    let parent = p ;
-    let choice =  d ;
-  
-     let lines = select('#plotGroup').selectAll('path');
-
-     let filterGroup = lines.filter(d => d[parent] == choice);
-
-     console.log(filterGroup);
-
-    filterGroup.attr('class', parent);
-
-    }
    
 
 export function create(parent:Element) {
