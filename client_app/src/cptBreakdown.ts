df9db943a11c6509816f0f31583bb55ef03b1e8b
Jen Rogers <jennifer.rogers1207@gmail.com>
2017-12-26 10:14:25 -0700

    rearranged data in cpt rects

--- client_app/src/cptBreakdown.ts
+++ client_app/src/cptBreakdown.ts
@@ -184,8 +184,6 @@ this.patOrderGroup = this.svg.select('#similar_cpt')
 
     events.fire('filtered_CPT', mapped);
 
- 
-
  };
 
   /**
@@ -207,30 +205,75 @@ this.patOrderGroup = this.svg.select('#similar_cpt')
               const self = this;
 
               // ----- add diff days to the data
-      
+            // console.log() similarOrdersInfo.values();
+              let filteredOrders = [];
               similarOrdersInfo.forEach((g) => {
-      
+
+                //g.array = [];
       
                  if(g.value != null){
                       let minDate = this.findMinDateCPT(g.value);
                  }
-          
                 // let minDate = this.findMinDate(similarOrdersInfo.value);
                   g.value.forEach((d) => {
+
+                      d.array = []; 
+                      d.time = d['PROC_DTM'];
+
                       try {
                           d.diff = Math.ceil((this.parseTime(d['PROC_DTM'], null).getTime() - minDate.getTime()) / (1000 * 60 * 60 * 24));
-                      }
+                          console.log(d.diff);
+                        }
                       catch (TypeError) {
                           console.log('error');
                           d.diff = -1;
                       }
-                  })
-              });
-      
+                      if(d['CPT_1'] !== 0){ d.array.push(d['CPT_1']);   };
+                      if(d['CPT_2'] !== 0){ d.array.push(d['CPT_2'])    };
+                      if(d['CPT_3'] !== 0){ d.array.push(d['CPT_3'])    };
+                      if(d['CPT_4'] !== 0){ d.array.push(d['CPT_4'])    };
+                      if(d['CPT_5'] !== 0){ d.array.push(d['CPT_5'])    };
+                      if(d['CPT_6'] !== 0){ d.array.push(d['CPT_6'])    };
+                      if(d['CPT_7'] !== 0){ d.array.push(d['CPT_7'])    };
+                      if(d['CPT_5'] !== 0){ d.array.push(d['CPT_5'])    };
+                      if(d['CPT_6'] !== 0){ d.array.push(d['CPT_6'])    };
+                      if(d['CPT_7'] !== 0){ d.array.push(d['CPT_7'])    };
+                      //console.log(g.array);
+                      d.diff = d.diff;
+
+                     });
+
+                     let filter = g.value.map(function(blob) {
+                        let temp = [];
+                        temp.push(blob.array);  
+                        return {
+                            key: blob.PAT_ID,
+                            value : temp,
+                            time: blob.PROC_DTM,
+                            diff : blob.diff
+                            };
+                        });
+/* attempted array of codes separate
+                        let filter = g.value.map(function(blob) {
+                            let temp = [];
+                            temp.push(blob.array);  
+                            return {
+                                key: blob.PAT_ID,
+                                values : temp,
+                                };
+                            });
+                        */
+                      //console.log(filter);
+                      filteredOrders.push(filter);
+                           
+                });
+                console.log(filteredOrders);
+                console.log(similarOrdersInfo);
             
               let patGroups = this.patOrderGroup
                   .selectAll('.patgroups')
-                  .data(similarOrdersInfo);
+                  .data(filteredOrders);
+                  //.data(similarOrdersInfo);
 
                   patGroups.exit().remove();
 
@@ -238,7 +281,7 @@ this.patOrderGroup = this.svg.select('#similar_cpt')
                   .enter()
                   .append('g').attr('class', 'patgroups')//.attr('class', d=> d.key);
               let patGroupText = similarOrderGroupEnter
-                  .append('text').text(d=> d.key)
+                  .append('text').text(d=>d[0].key)
                   .attr('transform', `translate(0,10)`);
 
                  similarOrderGroupEnter
@@ -248,18 +291,19 @@ this.patOrderGroup = this.svg.select('#similar_cpt')
                   .classed('similarRectCPT', true)
                   .attr('transform', `translate(60,0)`)
                   .selectAll('rect')
-                  .data(d=> d.value)
+                  .data((d, i)=> d)
+                //  .data(d=> d.value)
                   .enter()
                   .append('rect')
                   //.attr('class', (d) => `${d['ORDER_CATALOG_TYPE']}`)
-                  .attr('class', this.getClassAssignment('ORDER_CATALOG_TYPE'))
-                  .attr('class', this.getClassAssignment('ORDER_STATUS'))
+                  .attr('class', this.getClassAssignment('time'))
+                 // .attr('class', this.getClassAssignment('CPT_2'))
                   .attr('x', (g) => this.timeScale(g.diff))
                   .attr('y', 0)
                   .attr('width', this.similarBar.width)
                   .attr('height', this.similarBar.height)
                  
-             /*     .on("mouseover", (d) => {
+                 .on("mouseover", (d) => {
                       let t = transition('t').duration(500);
                       select(".tooltip")
                           .html(() => {
@@ -274,7 +318,7 @@ this.patOrderGroup = this.svg.select('#similar_cpt')
                       let t = transition('t').duration(500);
                       select(".tooltip").transition(t)
                           .style("opacity", 0);
-                  });*/
+                  });
 
                   patGroups = similarOrderGroupEnter.merge(patGroups);
                   
@@ -287,6 +331,27 @@ this.patOrderGroup = this.svg.select('#similar_cpt')
                   .attr('height', this.rectBoxDimension.height - 50 + (this.similarBar.height + 5) * similarOrdersInfo.length);
       
           }
+
+          //tooltip
+  private renderOrdersTooltip(tooltip_data) {
+        let text
+        text = tooltip_data['time'];
+        tooltip_data.value.forEach((d, i) => {
+            console.log(d[i]);
+        });
+        /*
+    if(tooltip_data['CPT_1'] !== 0){text = "<strong style='color:darkslateblue'>" + tooltip_data['CPT_1'] + "</strong></br>";}
+        if(tooltip_data['CPT_2'] !== 0){text += "<span>" + tooltip_data['CPT_2'] + "</span></br>"; }
+        if(tooltip_data['CPT_3'] !== 0){ text += "<span>" + tooltip_data['CPT_3'] + "</span></br>";}
+        if(tooltip_data['CPT_4'] !== 0){ text += "<span>" + tooltip_data['CPT_4'] + "</span></br>";}
+        if(tooltip_data['CPT_5'] !== 0){text += "<span>" + tooltip_data['CPT_5'] + "</span></br>";}
+        if(tooltip_data['CPT_6'] !== 0){ text = "<strong style='color:darkslateblue'>" + tooltip_data['CPT_6'] + "</strong></br>";}
+        if(tooltip_data['CPT_7'] !== 0){text += "<span>" + tooltip_data['CPT_7'] + "</span></br>";}
+        if(tooltip_data['CPT_8'] !== 0){ text += "<span>" + tooltip_data['CPT_8'] + "</span></br>";}
+        if(tooltip_data['CPT_9'] !== 0){text += "<span>" + tooltip_data['CPT_9'] + "</span></br>";}
+        if(tooltip_data['CPT_10'] !== 0){text += "<span>" + tooltip_data['CPT_10'] + "</span></br>";}*/
+        return text;
+      }
 
 
 
