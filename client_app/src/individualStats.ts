dbf83b6ddfa98aa4a97a5301c342789935022ec8
Jen <jen@sci.utah.edu>
2018-05-23 17:02:58 -0600

    added cpt rect line for patient on click

--- client_app/src/individualStats.ts
+++ client_app/src/individualStats.ts
@@ -13,26 +13,50 @@ import * as parallel from './parallel';
 import {scaleLinear,scaleTime,scaleOrdinal, scaleBand} from 'd3-scale';
 //importing other modules from app
 import * as demoGraph from './demoGraphs';
+import * as d3 from 'd3';
+import {extent, min, max, ascending} from 'd3-array';
+import {axisBottom,axisLeft} from 'd3-axis';
 
 export class individualStats {
 
     private $node;
-    private statWin
+    private statWin;
+    private rectWin;
+    private timeScale;
+    private rectBoxDimension;
+    private rectSvg;
 
     constructor(parent: Element) {
+
         this.$node = select(parent)
         .append('div')
         .classed('individualStatWrapper', true);
 
+        this.rectWin = this.$node.append('div').classed('patientCPTOrders', true);
         this.statWin = this.$node.append('div').classed('patientPromisOrders', true);
+       
+
+        this.rectBoxDimension = {'width' : 600, 'height': 30};
+
+        this.timeScale = scaleLinear()
+        .range([0, this.rectBoxDimension.width])
+        .clamp(true);
+
+        this.rectSvg = this.rectWin.append('svg')
+        .attr('width', this.rectBoxDimension.width)
+        .attr('height', this.rectBoxDimension.height);
+
         this.attachListener();
+
 
     }
 
     private attachListener () {
+        events.on('chosen_pat_cpt', (evt, item)=> {
+            this.drawPatRects(item);
+        });
+
         events.on('line_clicked', (evt, item)=> {
-            console.log(item);
-          //  this.statWin.selectAll('div').remove();
             this.drawOrderWin(item);
         });
         events.on('line_unclicked', (evt, item)=>{
@@ -61,6 +85,27 @@ export class individualStats {
             div.append('br');
         });
        
+    }
+
+    private drawPatRects(orders) {
+        this.timeScale.domain([-365, 365]);
+        let rectGroup = this.rectSvg.selectAll('.rect_group').data(orders).enter()
+        .append('g').classed('rect_group', true);
+
+        let rects = rectGroup.selectAll('.ind_order_rect').data(d=> d);
+
+        rects.exit().remove();
+
+        let rectsEnter = rects.enter().append('rect').classed('.ind_order_rect', true);
+
+        rects = rectsEnter.merge(rects);
+
+        rects//.attr('class', this.getClassAssignment('ORDER_STATUS'))
+        .attr('x', (g) => this.timeScale(g.diff))
+        .attr('y', 0)
+        .attr('width', 10)
+        .attr('height', 25);
+
     }
 
 }
