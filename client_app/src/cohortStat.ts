ea579bc9fdb94f1ab727c67d26433b18531fa7e9
Jen <jen@sci.utah.edu>
2018-06-25 21:30:32 -0600

    working on adding links

--- client_app/src/cohortStat.ts
+++ client_app/src/cohortStat.ts
@@ -49,8 +49,7 @@ export class CohortStat{
     private attachListener(){
 
         events.on('calculate_agg', (evt,item)=> {
-           
-           // console.log(this.$node);
+
             this.statWrapper.select('.statsAfterEvent').remove();
             this.getAverage(item);
         });
