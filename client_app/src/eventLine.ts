42f001a4a0ab0570db3d94428586e7ea42e3b495
Jen <jen@sci.utah.edu>
2018-08-08 18:50:41 -0600

    added a slider to adjust the range of days accounted for in the sep quant

--- client_app/src/eventLine.ts
+++ client_app/src/eventLine.ts
@@ -298,13 +298,10 @@ export class EventLine {
                         let rel = ul.append('li').attr('class', 'choice').append('text').text('Relative');//.attr('value', 'Absolute');
               
             abs.on('click', () =>{
-                          //  this.scoreLabel = 'Absolute Scale';
                             this.drawEventButtons(cohort);
-                           // this.drawScoreFilterBox(this.scoreBox);
                             events.fire('change_promis_scale', this.scoreLabel)});
     
             rel.on('click', () =>{
-                               // this.scoreLabel = 'Relative Scale';
                                 this.drawEventButtons(cohort);
                                 events.fire('change_promis_scale', this.scoreLabel)});
     
@@ -365,6 +362,8 @@ export class EventLine {
                         }
                     });
                     bCheck.append('label').attr('for', 'sampleB').text('bottom').style('color', '#fc8d59');
+
+                    /*
                     let radio = quartDiv.append('form');
                     radio.append('input').attr('type', 'radio').attr('value', true).attr('name', 'quart').attr('id', 'quartile-radio-1');
                     radio.append('label').attr('for', 'quartile-radio-1').text('Average Score Change');
@@ -372,22 +371,130 @@ export class EventLine {
                     radio.append('label').attr('for', 'quartile-radio-2').text('Score at Zero Day');
                     if(this.scoreChangeBool){
                         select(document.getElementById(this.scoreChangeBool)).attr('checked', true);
-                    }
-
+                    }*/
+           
+                   /*
                     this.$node.selectAll("input[name='quart']").on('change', function() {
                         let scoreChange = {id : this.id, scaleR: null};
                         if(this.id == 'quartile-radio-1'){ scoreChange.scaleR = true;
                         }else{ scoreChange.scaleR = false; }
                         that.scoreChangeBool = this.id;
                         events.fire('change_sep_bool', scoreChange);
+                    });*/
+
+                    let slidDiv = quartDiv.append('div').attr('id', 'sepSlider');
+
+                    var margin = {left: 30, right: 30},
+                    width = 200,
+                    height = 60,
+                    range = [0, 365],
+                    step = 2; // change the step and if null, it'll switch back to a normal slider
+            
+                    // append svg
+                    const sliderDiv = select('#sepSlider').node();
+                    
+                    const svg = select(sliderDiv).append('svg').node();
+
+                    select(svg).attr('width', width)
+                                    .attr('height', height);
+                
+                    const slider = select(svg).append('g').node();
+
+                    select(slider).classed('slider', true)
+                        .attr('transform', 'translate(' + margin.left +', '+ (height/2) + ')');
+
+                    // using clamp here to avoid slider exceeding the range limits
+                    var xScale = scaleLinear()
+                        .domain(range)
+                        .range([0, width - margin.left - margin.right])
+                        .clamp(true);
+                
+                    // array useful for step sliders
+                    var rangeValues = d3.range(range[0], range[1], step || 1).concat(range[1]);
+                    var xAxis = axisBottom(xScale).ticks(10);
+                  //  .tickFormat((d)=> {
+                     //   return d;
+                   ///});
+            
+                xScale.clamp(true);
+                // drag behavior initialization
+                const dragg = drag()
+                    .on('start.interrupt', function () {
+                        select(slider).interrupt();
+                    }).on('start drag', function () {
+                        dragged(event.x);
+        
                     });
 
+                
+                    // this is the main bar with a stroke (applied through CSS)
+                var track = select(slider).append('line').attr('class', 'track')
+                .attr('x1', xScale.range()[0])
+                .attr('x2', xScale.range()[1]);
+
+                var trackInset = select(slider).append('line').attr('class', 'track-inset')
+                .attr('x1', xScale.range()[0])
+                .attr('x2', xScale.range()[1]);
+
+                var trackOverlay = select(slider).append('g').attr('class', 'track-overlay')
+                .attr('x1', xScale.range()[0])
+                .attr('x2', xScale.range()[1])
+                
+
+                // this is a bar (steelblue) that's inside the main "track" to make it look like a rect with a border
+               // var trackInset = d3.select(slider.appendChild(track.node().cloneNode())).attr('class', 'track-inset');
+
+                var ticks = select(slider).append('g').attr('class', 'ticks').attr('transform', 'translate(0, 4)')
+                    .call(xAxis);
+
+                // drag handle
+                var handle = select(slider).append('circle').classed('handle', true)
+                    .attr('r', 8).call(dragg);
+
+                select(slider).transition().duration(750)
+                .tween("drag", function () {
+                   // var i = d3.interpolate(0, 10);
+                    return function (t) {
+                       // dragged(xScale(i(t)));
+                        dragged(xScale(t));
+                    }
+                });
+
+                function dragged(value) {
+                    var x = xScale.invert(value), index = null, midPoint, cx, xVal;
+                    console.log(x);
+                    if(step) {
+                        // if step has a value, compute the midpoint based on range values and reposition the slider based on the mouse position
+                        for (var i = 0; i < rangeValues.length - 1; i++) {
+                            if (x >= rangeValues[i] && x <= rangeValues[i + 1]) {
+                                index = i;
+                                break;
+                            }
+                        }
+                        midPoint = (rangeValues[index] + rangeValues[index + 1]) / 2;
+                        if (x < midPoint) {
+                            cx = xScale(rangeValues[index]);
+                            xVal = rangeValues[index];
+                        } else {
+                            cx = xScale(rangeValues[index + 1]);
+                            xVal = rangeValues[index + 1];
+                        }
+                    } else {
+                        // if step is null or 0, return the drag value as is
+                        cx = xScale(x);
+                        xVal = x.toFixed(3);
+                    }
+                    // use xVal as drag value
+                    handle.attr('cx', cx);
+                
+                }
+/*
                     if(!separated){  
                         checkDiv.classed('hidden', true); 
                         radio.classed('hidden', true); }
                     else{ checkDiv.classed('hidden', false); 
                           radio.classed('hidden', false);}
-
+*/
                     if(this.layerBool){
                         //document.getElementById('quartile-btn').classList.add('disabled');
                         quartDiv.select('#quartile-btn').classed('disabled', true);
